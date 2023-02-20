ce699814bccbee3369f4380738f57538fdd5ca05
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2023-02-20 21:37:27 +0700

    Feature: Now Probably Working. Gotta test on the boards tho.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,59 @@
 extern crate serde;
 extern crate serde_json;
 
-use std::collections::HashMap;
+use std::io;
 use std::net::UdpSocket;
+
 use connection::TagData;
+use triangulation::Triangulator;
 
 mod triangulation;
 mod connection;
 
 fn main() {
-    let mut anchors: HashMap<String, TagData> = HashMap::new();
+    let anchor_dist = get_anchor_distance();
+    let mut triangulator = Triangulator::from(anchor_dist);
     let socket = UdpSocket::bind("0.0.0.0:8888")
         .unwrap();
     
+    triangulator.init(); 
+    // UDP loop
     loop {
         let mut buf = [0; 256];
         socket.recv_from(&mut buf).unwrap();
         
-        let data = match connection::parse_udp(&buf) {
+        let tag_data = match connection::parse_udp(&buf) {
             Ok(v) => v,
             Err(e) => {
                 println!("Failed to parse JSON: {}", e);
                 continue;
             },
         };
-        let data_source = String::from(&data.source);
         
-        if let None = anchors.insert(data_source.clone(), data) {
-            println!("Added new anchor: {}", data_source.clone());
-        }
-        
-        triangulation::update_position(&anchors);
+        triangulator.update_anchor_info(tag_data);
     }
+}
+
+/// Gets the distance between anchors from the user
+fn get_anchor_distance() -> f64 {
+    // Setting the distance between anchors
+    let mut anchor_dist_buf = String::new();
+    let anchor_dist: f64;
+    
+    // Anchor Distance Input Loop
+    loop {
+        println!("Please Input the distance between the anchors (in meters):");
+        io::stdin()
+            .read_line(&mut anchor_dist_buf)
+            .expect("Failed to read line");
+        
+        anchor_dist = match anchor_dist_buf.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        break;
+    }
+    
+    anchor_dist
 }
--- src/triangulation.rs
+++ src/triangulation.rs
@@ -1,19 +1,115 @@
 use std::collections::HashMap;
-
-use turtle::Point;
-
+use turtle::{Point, Turtle, Drawing};
 use crate::TagData;
 
-pub fn triangulate_coordinate(distance_between_anchors: f64, tag_to_anchor_1: f64, tag_to_anchor_2: f64) -> Point {
-    let a = distance_between_anchors;
-    let b = tag_to_anchor_1;
-    let c = tag_to_anchor_2;
-    
-    let cos_a = ((b * b) + (c * c) - (a * a)) / (2.0 * b * c);
-    let x = b * cos_a;
-    let y = b * f64::sqrt(1.0 - (cos_a * cos_a));
-    
-    Point { x, y }
+pub struct Triangulator {
+    point: Point,
+    drawing: Drawing,
+    turtle: Turtle,
+    anchors: HashMap<String, TagData>,
+    anchor_names: Vec<String>,
+    anchor_dist: f64,
 }
 
-pub fn update_position(anchors: &HashMap<String, TagData>) {}
+impl Triangulator {
+    pub fn init(&mut self) {
+        self.turtle.pen_up();
+        self.turtle.go_to(self.point);
+        
+        self.turtle.pen_down();
+        
+        self.turtle.go_to(Point { x: self.point.x * -1.0, y: self.point.y });
+        self.turtle.go_to(Point { x: self.point.x * -1.0, y: self.point.y * -1.0 });
+        self.turtle.go_to(Point { x: self.point.x, y: self.point.y * -1.0 });
+        self.turtle.go_to(self.point);
+        
+        self.turtle.pen_up();
+    }
+    
+    fn triangulate_coordinates(&self) -> Option<Point> {
+        let anchor_1 = self.anchors.get(self.anchor_names.get(0).unwrap()).unwrap();
+        let anchor_2 = self.anchors.get(self.anchor_names.get(1).unwrap()).unwrap();
+        
+        if anchor_1.range < 0.0 || anchor_2.range < 0.0 || anchor_1.range + anchor_2.range <= self.anchor_dist {
+            return None;
+        }
+ 
+        let a = self.anchor_dist;
+        let b = anchor_1.range;
+        let c = anchor_2.range;
+        
+        let cos_a = ((b * b) + (c * c) - (a * a)) / (2.0 * b * c);
+        let x = b * cos_a;
+        let y = b * f64::sqrt(1.0 - (cos_a * cos_a));
+        
+        Some(Point { x, y })
+    }
+    
+    pub fn update_anchor_info(&mut self, tag_data: TagData) {
+        if self.anchor_names.len() >= 2 {
+            println!("Cannot use more than 2 anchors");
+            return;
+        }
+        
+        let tag_data_source = tag_data.source.clone();
+        if let None = self.anchors.insert(tag_data.source.clone(), tag_data) {
+            println!("Added New Anchor: {}", &tag_data_source);
+            self.anchor_names.push(tag_data_source);
+        }
+        
+        self.update_display();
+    }
+    
+    fn update_display(&mut self) {
+        if self.anchor_names.len() != 2 {
+            return;
+        }
+        
+        self.turtle.go_to(self.point);
+        
+        let position = self.triangulate_coordinates();
+        let canvas_size = self.drawing.size();
+        let scale = (canvas_size.width - 200) as f64 / self.anchor_dist;
+        
+        match position {
+            Some(pos) => {
+                let x_offset = ((canvas_size.width - 200) as f64 / 2.0) * -1.0;
+                let y_offset = (canvas_size.height - 200) as f64 / 2.0;
+                
+                let scaled_pos = Point {
+                    x: x_offset + pos.x * scale,
+                    y: y_offset - pos.y * scale,
+                };
+                
+                self.turtle.go_to(scaled_pos);
+                self.point = scaled_pos;
+            },
+            None => (),
+        };
+         
+        println!("{:#?}", position);
+    }
+    
+}
+
+impl From<f64> for Triangulator {
+    fn from(item: f64) -> Self {
+        let mut drawing = Drawing::new();
+        let mut turtle = drawing.add_turtle();
+        turtle.set_speed("instant");
+        
+        let canvas_size = drawing.size();
+        let x = ((canvas_size.width - 200) as f64 / 2.0) * -1.0;
+        let y = (canvas_size.height - 200) as f64 / 2.0;
+        let initial_point = Point { x, y };
+        
+        Triangulator {
+            point: initial_point,
+            drawing,
+            turtle,
+            anchors: HashMap::new(),
+            anchor_names: Vec::new(),
+            anchor_dist: item
+        }
+    }
+}
